da43a47c23fc216f1161d81679d183fbfa76ab00
soulis <sdi2300122@di.uoa.gr>
2025-10-28 13:31:04 +0200

    Close and Insert v1

--- Heap-File-Exercise/src/hp_file.c
+++ Heap-File-Exercise/src/hp_file.c
@@ -59,7 +59,7 @@ int HeapFile_Open(const char *fileName, int *file_handle, HeapFileHeader** heade
   memcpy(&temp_header, data, sizeof(HeapFileHeader));
 
   *header_info = malloc(sizeof(HeapFileHeader));
-  if(header_info == NULL) {}
+  if(*header_info == NULL) {return 0;}
   memcpy(*header_info, &temp_header, sizeof(HeapFileHeader));
   
   CALL_BF(BF_UnpinBlock(block));
@@ -70,11 +70,40 @@ int HeapFile_Open(const char *fileName, int *file_handle, HeapFileHeader** heade
 
 int HeapFile_Close(int file_handle, HeapFileHeader *hp_info)
 {
+  BF_Block* block;
+  BF_Block_Init(&block);
+  int blockNum;
+  CALL_BF(BF_GetBlockCounter(file_handle, &blockNum))
+  int i = (blockNum - 1);
+  for(i; i >= 0; i--) {  //gia kathe block tou Heap File
+    CALL_BF(BF_GetBlock(file_handle, i, block));    //pare to block kai sbhsto
+    BF_Block_Destroy(&block);
+  }
+  free(hp_info);  //free to header???
   return 1;
 }
 
 int HeapFile_InsertRecord(int file_handle, HeapFileHeader *hp_info, const Record record)
 {
+  BF_Block* block;
+  BF_Block_Init(&block);
+  int blockNum;
+  CALL_BF(BF_GetBlockCounter(file_handle, &blockNum));   //pare arithmo blocks
+  int BlockRecCount = BF_BLOCK_SIZE / sizeof(Record);   //posa records xorane ana block (edo 8)
+  int lastBlockRec = hp_info->recordCount % BlockRecCount;  //posa records xorane sto teleutaio block
+  int has_space = lastBlockRec < BlockRecCount ? 1 : 0;   //an teleutaio exei ligotera apo 8 records exei xoro
+  if (has_space){
+    CALL_BF(BF_GetBlock(file_handle, (blockNum - 1), block));  //pare teleutaio block kai bale to record
+    char* data = BF_Block_GetData(block);
+    memcpy(data + lastBlockRec*sizeof(Record), &record, sizeof(Record));
+    BF_Block_SetDirty(block);
+  } else {
+    CALL_BF(BF_AllocateBlock(file_handle, block));  //allocate kainourgio block
+    char* data = BF_Block_GetData(block); //pare data kai bale to record
+    memcpy(data, &record, sizeof(Record));
+    BF_Block_SetDirty(block);
+  }
+  BF_Block_Destroy(&block);
   return 1;
 }
 
